d2d8f3ba3ee4dea61e0c8e8a9359c65a43bcf647
Ovi Savescu <ovi@ovswebsites.com>
2021-03-27 19:47:30 +0200

    steps section and footer updates

--- src/utils/serializers.js
+++ src/utils/serializers.js
@@ -6,6 +6,7 @@ module.exports = {
     authorReference: ({node}) => `[${node.name}](/authors/${node.slug.current})`,
     code: ({node}) =>
       '```' + node.language + '\n' + node.code + '\n```',
+    iframeEmbed: ({node}) => node.code,
     mainImage: ({node}) => {
 
       if (node.caption) {
